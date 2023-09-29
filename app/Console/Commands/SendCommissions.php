f0277d7b3f5e725644690ecd6e2487ff19ecee7b
Raphael Lorencini <raphaellorencini@gmail.com>
2023-09-29 00:24:01 -0300

    Fix command

--- app/Console/Commands/SendCommissions.php
+++ app/Console/Commands/SendCommissions.php
@@ -2,7 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Mail\ComissionSendMail;
+use App\Repositories\SaleRepository;
+use App\Repositories\UserRepository;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class SendCommissions extends Command
 {
@@ -25,6 +30,36 @@ class SendCommissions extends Command
      */
     public function handle()
     {
-        //
+        $salesRepository = new SaleRepository(new UserRepository());
+        $users = DB::table('users as u')
+            ->select('u.*')
+            ->leftJoin('users_roles as ur', 'ur.user_id', '=','u.id')
+            ->leftJoin('roles as r', 'r.id', '=', 'ur.role_id')
+            ->whereNull('deleted_at')
+            ->get();
+        foreach ($users as $user) {
+            $this->sendMail($salesRepository, $user);
+        }
+
+        $this->sendMail($salesRepository);
+    }
+
+    public function sendMail($salesRepository, $user = null)
+    {
+        $date = now();
+        $datacommission = $salesRepository->commission(sellerId: $user?->id, date: $date);
+        $commission = $datacommission->commission;
+        $sales = $datacommission->sales;
+        $data = [
+            'subject' => 'Comissão - Data: '.$date->format('d/m/Y'),
+            'name' => $user?->name,
+            'email' => $user?->email,
+            'commission' => number_format($commission, 2, ",", "."),
+            'sales' => number_format($sales, 2, ",", "."),
+            'date' => $date,
+        ];
+
+        $to = $user?->email ?? env('MAIL_TO');
+        Mail::to($to)->send(new ComissionSendMail($data));
     }
 }
